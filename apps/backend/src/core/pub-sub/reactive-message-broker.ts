36385aca9d052a07f10cfe083c5d2a1cd85785a1
mario <codemario318@gmail.com>
2025-01-22 20:38:47 +0900

    feat: 메시지 브로커 구현체 추가
    - RxJS를 활용한 메시지 브로커 구현

---
+++ apps/backend/src/core/pub-sub/reactive-message-broker.ts
@@ -0,0 +1,56 @@
+import { Subject } from 'rxjs';
+import { Message } from './interfaces/message.interface';
+import { PubSub } from './interfaces/pub-sub.interface';
+
+export class ReactiveMessageBroker<TTopic, TData> implements PubSub<TTopic, TData> {
+    constructor(
+        private publishers: Map<string, Subject<Message<TTopic, TData>>> = new Map(),
+    ) {}
+
+    public async addPublisher(id: string) {
+        if (this.publishers.has(id)) {
+            throw new Error(`Publisher with ID ${id} already exists`);
+        }
+
+        this.publishers.set(id, new Subject<Message<TTopic, TData>>());
+    }
+
+    public async removePublisher(id: string) {
+        const publisher = this.publishers.get(id);
+        if (!publisher) {
+            throw new Error(`Publisher with ID ${id} does not exist`);
+        }
+
+        publisher.complete();
+        this.publishers.delete(id);
+    }
+
+    public async publish(id: string, message: Message<TTopic, TData>) {
+        const publisher = this.publishers.get(id);
+
+        if (!publisher) {
+            throw new Error(`Publisher with ID ${id} does not exist`);
+        }
+
+        publisher.next(message);
+    }
+
+    public async subscribe(
+        publisherId: string,
+        subscriberId: string,
+        handler: (message: Message<TTopic, TData>) => void
+    ) {
+        const publisher = this.publishers.get(publisherId);
+
+        if (!publisher) {
+            throw new Error(`Publisher with ID ${publisherId} does not exist`);
+        }
+
+        const subscription = publisher.subscribe({
+            next: handler,
+            error: (error) => console.error(`Error in subscription ${publisherId}:${subscriberId} :`, error)
+        });
+
+        return async () => subscription.unsubscribe();
+    }
+}
